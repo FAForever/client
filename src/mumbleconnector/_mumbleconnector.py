4259eefce88f0b509562f2582322dd01fa82909a
rbroekstra <rien@rename-it.nl>
2013-03-03 16:37:50 +0100

    Removed relinking mumble because the library doesnt seem to support that Removed lobby chat feature because of invalid gameinfo messages

--- src/mumbleconnector/_mumbleconnector.py
+++ src/mumbleconnector/_mumbleconnector.py
@@ -37,20 +37,13 @@ class mumbleConnector():
         logger.info("MumbleConnector instantiated.")
 
     def link_mumble(self):
-        # Check if there already is a mumble link thread running
-        if self.mumbleLinkActive:
-             mumble_link.stop()
-             self.mumbleLinkActive = None
-             logger.info("Resetting Mumble Link")
-                
-        # Launch Mumble
+        # If the link failed, mumble is probably not yet running. Run it.
         url = QtCore.QUrl()
         url.setScheme("mumble")
         url.setHost(self.mumbleHost)
         url.setPath(self.mumbleChannelRoot)
         url.addQueryItem("version", "1.2.0")
             
-        # Launch mumble, and connect it to the faforever server
         # FIXME: Should we make the path configurable?
         workingdir_x86 = os.path.join('c:', os.sep, 'Program Files', 'Mumble')
         executable_x86 = os.path.join(workingdir_x86, 'mumble.exe')
@@ -70,70 +63,86 @@ class mumbleConnector():
             executable = None
             workingdir = None
             logger.info("Mumble installation not found")
-
-        if executable:
-            logger.info("Launching mumble: " + executable + " " + url.toString())
-            win32api.ShellExecute(0, "open", executable, url.toString(), workingdir, 4) # 4 == SW_SHOWNOACTIVATE == start normal, inactive
-
-            # Connect with mumble_link
-            for i in range (1,5):
+            return
+        
+        logger.info("Launching mumble: " + executable + " " + url.toString())
+        win32api.ShellExecute(0, "open", executable, url.toString(), workingdir, 4) # 4 == SW_SHOWNOACTIVATE == start normal, inactive
+            
+        # Connect with mumble_link
+        for i in range (1,10):
+            if mumble_link.setup("faforever", "The Forged Alliance Forever Lobby Channel Placement Plugin"):
                 logger.info("Trying to connect link plugin: " + str(i))
-                if mumble_link.setup("faforever", "The Forged Alliance Forever Lobby Channel Placement Plugin"):
-                    logger.info("Mumble link established")
-                    self.mumbleLinkActive = 1
-                    return
-                time.sleep(i)
-
-            logger.info("Mumble link failed")
-
-    # When we get noticed the user quit FA, check if we are in a voice channel. If so, move us to the (silent) lobby channel
+                logger.info("Mumble link established")
+                self.mumbleLinkActive = 1
+                return
+            time.sleep(i)
+            
+        logger.info("Mumble link failed")
+                
     def processGameExit(self):
-        # We use the context-information to transmit the gameuid, and the identity-information to transmid the teamnumber.
-        # A plugin on the server-side will pick these state changes up, and put us into the correct channel
+        self.state = "closed"
         if self.mumbleLinkActive:
             logger.debug("Sending state change to mumble client")
             mumble_link.set_identity("0")
-                    
+            
     def processGameInfo(self, gameInfo):
+        if self.playerInTeam(gameInfo):
+            self.functionMapper[gameInfo["state"]](self, gameInfo)
+            return
+
+        # Check whether this gameInfo signal is about the last game we
+        # were known to be in
+        # if gameInfo["uid"] == self.uid:
+        #    self.functionMapper[gameInfo["state"]](self, gameInfo)
+
+
+    #
+    # Helper function to determine if we are in this gameInfo signal's team
+    #
+    def playerInTeam(self, gameInfo):
         # Check whether this gameInfo signal is about a game we are in
         for team in gameInfo["teams"]:
             for player in gameInfo['teams'][team]:
                 if self.client.login == player:
+                    logger.debug("We think we are in this game:")
+                    logger.debug("self.client.login: " + str(self.client.login))
+                    logger.debug("gameInfo['teams']: " + str(gameInfo['teams']))
+                    logger.debug("self.uid: " + str(self.uid))
+                    logger.debug("gameInfo['uid']: " + str(gameInfo['uid']))
+                    logger.debug(str(gameInfo))
+                    return 1
 
-                    # If we're in this game, execute the state transition
-                    # corresponding to the current game state
-                    self.functionMapper[gameInfo["state"]](self, gameInfo)
-                    return
+        return 0
 
-        # Check whether this gameInfo signal is about the last game we
-        # were known to be in
-        if gameInfo["uid"] == self.uid:
-            self.functionMapper[gameInfo["state"]](self, gameInfo)
-
+    #
+    # Process a state transition to state "open"
+    #
     def state_open(self, gameInfo):
         if gameInfo["uid"] > self.uid:
 
-            # This regularly puts us in a channel we're not supposed to be in!!
+            # FIXME: We regularly get gameInfo signal with ourselves in the
+            # teamlist, when we are not in that game! Bug in server?
 
             # Player started a new lobby.
             self.state = "open"
             self.uid = gameInfo["uid"]
 
-            logger.debug(str(gameInfo))
 
-            # This is probably a good time to re-check the status of our mumble connection
-            # self.link_mumble()
-
+            # FIXME: Since we get false signals that we entered a lobby, we disable the chat-in-lobby feature
             # And join to this game's lobby channel
             # if self.mumbleLinkActive:
             #   logger.debug("Sending state change to mumble client")
             #    mumble_link.set_identity(str(gameInfo["uid"]) + "-0")
 
+    #
+    # Process a state transition to state "playing"
+    #
     def state_playing(self, gameInfo):
 
         # Check if our game just launched
-        if self.state != "playing" and self.uid == gameInfo["uid"]:
+        if self.state != "playing":
             self.state = "playing"
+            self.uid = gameInfo["uid"]
 
             logger.debug(gameInfo["teams"])
 
@@ -146,25 +155,29 @@ class mumbleConnector():
                 # Ignore 1-person-teams
                 if len(gameInfo['teams'][team]) < 2:
                     logger.debug("Not putting 1 person team " + team + " in a channel")
-                    continue
+                    #continue (don't do this for testing purposes)
                 
                 for player in gameInfo['teams'][team]:
                     if self.client.login == player:
                         myTeam = team
 
-            if myTeam != "0":
-                # We use the context-information to transmit the gameuid, and the identity-information to transmid the teamnumber.
-                # A plugin on the server-side will pick these state changes up, and put us into the correct channel
                 if self.mumbleLinkActive:
                     logger.debug("Sending state change to mumble client")
-                    mumble_link.set_identity(str(gameInfo["uid"]) + "-" + str(team))
+                    
+                    # We use the context-information to transmit the gameuid, and the identity-information to transmid the teamnumber.
+                    # A plugin on the server-side will pick these state changes up, and put us into the correct channel
+                    if myTeam != "0":
+                        mumble_link.set_identity(str(gameInfo["uid"]) + "-" + str(team))
+                    else:
+                        mumble_link.set_identity("0")
                 else:
-                    logger.debug("No mumble")
+                    logger.debug("Mumble is not linked")
                 
+    #
+    # Process a state transition to state "closed"
+    #
     def state_closed(self, gameInfo):
-        # Check if this is a transistion from playing to clsoed
-        if self.state == "playing" and self.uid == gameInfo["uid"]:
-            self.state = "closed"
+        pass
 
     # Mapper dictionary from gameInfo states to state transition functions
     functionMapper = {"open" : state_open,
