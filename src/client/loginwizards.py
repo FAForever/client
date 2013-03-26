7e20f9f20b3457537e386a0518ed875abe7a2376
nozon@DOUBLEEIGHT01 <nozon@DOUBLEEIGHT01>
2013-03-26 11:12:45 +0100

    Valid port range.

--- src/client/loginwizards.py
+++ src/client/loginwizards.py
@@ -388,7 +388,7 @@ class GameSettings(QtGui.QWizardPage):
         
         self.gamePortSpin = QtGui.QSpinBox() 
         self.gamePortSpin.setMinimum(1024)
-        self.gamePortSpin.setMaximum(9999999) 
+        self.gamePortSpin.setMaximum(65535) 
         self.gamePortSpin.setValue(6112)
 
         self.checkUPnP = QtGui.QCheckBox("use UPnP")
