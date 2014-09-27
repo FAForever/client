4a1e7478866d9aa072a9c889d56b3ff296b84ebb
Thygrrr <mail@thygrrr.de>
2014-09-27 23:52:56 +0200

    Test commit for setup.py new versioning.

--- setup.py
+++ setup.py
@@ -1,91 +1,19 @@
-# -------------------------------------------------------------------------------
-# Copyright (c) 2012 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
-
-
-# A simple setup script to create various executables with
-# different User Access Control flags in the manifest.
-
-# Run the build process by entering 'setup.py py2exe' or
-# 'python setup.py py2exe' in a console prompt.
-#
-# If everything works well, you should find a subdirectory named 'dist'
-# containing lots of executables
-from setuptools import setup, find_packages
-import py2exe
-import shutil
-import os
-import matplotlib
 import sys
+from cx_Freeze import setup, Executable
 
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"include_files": ["res"], "icon": "res/faf.ico"}
 
-# The targets to build
-if os.path.isfile("build.dat"):
-    with open("build.dat") as f:
-        BUILD = int(f.read()) + 1
-else:
-    BUILD = 1
-
-print "py2exe version: " + py2exe.__version__
-print "BUILD: " + str(BUILD)
-# create a target that says nothing about UAC - On Python 2.6+, this
-# should be identical to "asInvoker" below.  However, for 2.5 and
-# earlier it will force the app into compatibility mode (as no
-# manifest will exist at all in the target.)
-
-t1 = {
-    "script": "src/__main__.py",
-    "dest_base": "FAForever",
-    "icon_resources": [(0, "res/faf.ico")]
-}
-
-print "BUILD BEGINS."
-
-if (os.path.isdir("dist")):
-    shutil.rmtree("dist")
-
-shutil.copytree("lib", "dist")  #Lib directory needs to contain MSVCRT90.dll, plus Qt Image format plugins etc.
-shutil.copytree("res", "dist/res")
-
-VERSION_STRING = "0.10." + str(BUILD)
-
-versionfile = open("dist/version", "w")
-versionfile.write(VERSION_STRING)
-versionfile.flush()
-os.fsync(versionfile.fileno())
-versionfile.close()
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
 
 setup(
-    windows=[t1],  # targets to build
-    version=VERSION_STRING,
-    description="Forged Alliance Forever",
-    name="Forged Alliance Forever",
-    options={
-        "py2exe": {
-            "includes": ["ctypes.util", "sip", "PyQt4"],
-            "dll_excludes": ["MSVCP90.dll", "POWRPROF.dll", "API-MS-Win-Core-LocalRegistry-L1-1-0.dll", "MPR.dll"],
-            'excludes': ['_gtkagg', '_tkagg', "OpenGL"],
-        }
-    },
-    data_files=matplotlib.get_py2exe_datafiles(),
-    zipfile="FAForever.lib", requires=['sip']
+    name="faforever",
+    version="0.1",
+    description="Forged Alliance Forever!",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("src/__main__.py", base=base, targetName="FAForever.exe")]
 )
-
-open("build.dat", "w").write(str(BUILD))
-
-print "BUILD FINISHED!"
-print "Build no. " + str(BUILD)
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -30,23 +30,16 @@ from ctypes import *
 def developer():
     return sys.executable.endswith("python.exe")
 
+try:
+    with open("version", "r") as version_file:
+            VERSION_STRING = version_file.read
+            VERSION = int(VERSION_STRING.rsplit('.', 1)[1])
+except (BaseException, IOError), e:
+    VERSION = 0
+    VERSION_STRING = "undefined"
 
 LOGFILE_MAX_SIZE = 256 * 1024  #256kb should be enough for anyone
 
-#Load build number from version file.
-if not developer():
-    #We must operate from our current directory
-    os.chdir(os.path.dirname(sys.executable))
-
-    try:
-        VERSION_STRING = open("version").read()
-        VERSION = int(VERSION_STRING.rsplit('.', 1)[1])
-    except:
-        VERSION = 0
-        VERSION_STRING = "undefined"
-else:
-    VERSION = 0
-    VERSION_STRING = "development"
 
 UNITS_PREVIEW_ROOT = "http://www.faforever.com/faf/unitsDB/icons/big/"
 
