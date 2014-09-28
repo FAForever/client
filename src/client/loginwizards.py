6faf85240eafe567a6228fe5333198abbec7977e
Thygrrr <mail@thygrrr.de>
2014-09-28 20:30:41 +0200

    Refactor of URL constants and introduction of gitreports.com bug report page (allows anonymous reports)

--- src/client/loginwizards.py
+++ src/client/loginwizards.py
@@ -30,8 +30,8 @@ from client import ClientState, logger
 
 PASSWORD_RECOVERY_URL = "http://www.faforever.com/faf/forgotPass.php"
 NAME_CHANGE_URL = "http://www.faforever.com/faf/userName.php"
-STEAM_LINK_URL = "http://www.faforever.com/faf/steam.php"
-TICKET_URL = "http://www.faforever.com/forums/viewforum.php?f=3"
+
+from . import TICKET_URL, STEAMLINK_URL, NAME_CHANGE_URL, PASSWORD_RECOVERY_URL
 
 class LoginWizard(QtGui.QWizard):
     def __init__(self, client):
@@ -171,7 +171,7 @@ class loginPage(QtGui.QWizardPage):
 
     @QtCore.pyqtSlot()
     def linkAccount(self):
-        QtGui.QDesktopServices.openUrl(QtCore.QUrl(STEAM_LINK_URL))
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl(STEAMLINK_URL))
         
     @QtCore.pyqtSlot()
     def renameAccount(self):
