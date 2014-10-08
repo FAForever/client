ef06bf27c0d27e7afe3cad00d082254f09473d83
Thygrrr <mail@thygrrr.de>
2014-10-08 19:46:33 +0200

    Fixing split environment.

--- .travis.sh
+++ .travis.sh
@@ -1,38 +1,17 @@
-#!/bin/bash
-# this will install everything as root, so take that into account before you run it
+#!/bin/sh
 
-# need cmake, python development headers, ZLib and OpenSSL
-sudo apt-get install cmake python2.7-dev zlib1g-dev libssl-dev
+sudo apt-get install cmake
+pip install cffi
 
-mkdir libgit && cd libgit
+cd ~
 
-git clone git://github.com/libgit2/libgit2.git
+git clone --depth=1 -b v0.21.1 https://github.com/libgit2/libgit2.git
+cd libgit2/
 
-cd libgit2
-
-# instructions from http://libgit2.github.com/#install
 mkdir build && cd build
-cmake ..
-cmake --build .
-sudo cmake --build . --target install
+cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
+cmake --build . --target install
 
-cd ../../
+ls -la ..
 
-# instructions from https://github.com/libgit2/pygit2/blob/master/README.rst
-git clone git://github.com/libgit2/pygit2.git
-cd pygit2
-sudo python setup.py install
-
-# avoid "cannot open shared object file" ImportErrors; make sure the libraries are accessible in LD_LIBRARY_PATH
-# if you want a more permanent solution, try echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH` >> ~/.bashrc
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-
-# clean up after ourselves
-cd ../../
-rm -rf libgit/
-
-# now, test-run from within the python interpreter:
-python -c "import pygit2"
-
-#get other needed python packages
 sudo apt-get install python-qt4
