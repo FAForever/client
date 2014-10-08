513d1d2e29cb0b1689f2532d051cf5eb5da770f7
Thygrrr <mail@thygrrr.de>
2014-10-08 19:46:33 +0200

    Different way to set up environment for CI

--- .travis.sh
+++ .travis.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
+sudo apt-get install cmake
+pip install cffi
+
 cd ~
 
 git clone --depth=1 -b v0.21.1 https://github.com/libgit2/libgit2.git
 cd libgit2/
 
 mkdir build && cd build
-cmake ..
-cmake --build .
-cmake --target install
+cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
+cmake --build . --target install
 
-cd ~
+ls -la ..
 
 sudo apt-get install python-qt4
