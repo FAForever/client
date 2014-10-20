5687814e3536c68c5c2051b2839715581d5d8fe1
Thygrrr <mail@thygrrr.de>
2014-10-20 23:56:29 +0200

    Init Luas are loaded from bin directory again (upstream change came into develop, thus needed reverting)

--- src/fa/play.py
+++ src/fa/play.py
@@ -45,9 +45,12 @@ def build_argument_list(game_info, port, arguments=None):
         raise ValueError("Custom init scripts no longer supportes.")
 
     # Proper mod loading code, but allow for custom init by server
-    mods.fix_init_luas()
-    arguments.append("/init")
-    arguments.append(mods.init_lua_for_featured_mod(game_info['featured_mod']))
+    if not '/init' in arguments:
+        arguments.append('/init')
+        arguments.append("init_" + game_info['featured_mod'] + ".lua")
+    else:
+        #HACK for feature/new-patcher
+        raise ValueError("Server has sent a custon init command and I have no idea what to do with it.")
 
     #log file
     if settings.value("fa.write_game_log", DEFAULT_WRITE_GAME_LOG, type=bool):
