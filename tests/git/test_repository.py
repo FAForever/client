da2f16fd5d747acec77cb8d0354a341ce58f4ff7
Michael Sondergaard <msond@cs.au.dk>
2014-10-25 02:13:57 +0200

    Add has_version

--- tests/git/test_repository.py
+++ tests/git/test_repository.py
@@ -2,7 +2,7 @@ import pytest
 import py
 import os
 import pygit2
-from git import Repository
+from . import Repository, Version
 
 __author__ = 'Thygrrr'
 
@@ -155,3 +155,7 @@ def test_retrieves_correct_hex_on_branch_checkout(prefetched_repo):
     prefetched_repo.checkout(TEST_BRANCH)
     assert prefetched_repo.current_head.hex == TEST_BRANCH_COMMIT
 
+
+def test_repo_has_version(prefetched_repo):
+    prefetched_repo.checkout(TEST_TAG)
+    assert prefetched_repo.has_version(Version("thygrrr/test", TEST_TAG, None, TEST_TAG_COMMIT))
