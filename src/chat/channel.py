2ea6063d80c103b7bb5c5b29ac8fc0b7d95532ea
fafafaf <fafafaf>
2014-11-16 10:35:11 +0100

    Logic inverted, parameter renamed.

--- src/chat/channel.py
+++ src/chat/channel.py
@@ -179,11 +179,11 @@ class Channel(FormClass, BaseClass):
                 
             
     @QtCore.pyqtSlot()    
-    def pingWindow(self, silent=False):
+    def pingWindow(self, sound=True):
         QtGui.QApplication.alert(self.lobby.client)
             
         
-        if not silent and (not self.isVisible() or QtGui.QApplication.activeWindow()) != self.lobby.client:
+        if sound and (not self.isVisible() or QtGui.QApplication.activeWindow()) != self.lobby.client:
             if self.oneMinuteOrOlder():
                 if self.lobby.client.soundeffects:
                     util.sound("chat/sfx/query.wav")
@@ -276,7 +276,7 @@ class Channel(FormClass, BaseClass):
             if self.private and name != self.lobby.client.login:
                 self.pingWindow()
             elif not self.name in self.lobby.crucialChannels:
-                self.pingWindow(silent=True)
+                self.pingWindow(sound=False)
 
             if not self.private and text.find(self.lobby.client.login)!=-1:
                 self.pingWindow()
