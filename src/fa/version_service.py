9d3a9ad207ba115bc3fd4b2d26015ff3fdc07945
Michael Sondergaard <msond@cs.au.dk>
2014-11-17 10:31:21 +0100

    Document VersionService

--- src/fa/version_service.py
+++ src/fa/version_service.py
@@ -12,10 +12,23 @@ logger.setLevel(logging.DEBUG)
 
 
 class VersionService(IRESTService):
+    """
+    Service to get available versions for mods, maps etc.
+
+    TODO:
+        - Use for setting the version of a hosted game,
+          potentially within GameService
+    """
     def __init__(self, network_manager):
         IRESTService.__init__(self, network_manager)
 
     def versions_for(self, mod):
+        """
+        Get available versions for the given mod.
+
+        :param mod: identifier for the mod, eg 'faf', 'blackops'
+        :return: List of versions available of the mod
+        """
         url = VERSION_SERVICE_URL + "/default/"+mod
         logger.debug("Getting default versions from: " + url)
         return self._get(url)
