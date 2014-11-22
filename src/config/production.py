628b0d68e6c1cee4d9e96da8def5efb28d580ccb
Michael Sondergaard <msond@cs.au.dk>
2014-11-22 20:50:46 +0100

    Move client URL's into config

--- src/config/production.py
+++ src/config/production.py
@@ -22,5 +22,21 @@ defaults = {
     'PROXY': {
         'HOST': 'proxy.faforever.com',
         'PORT': 9134
-    }
+    },
+    'LOBBY': {
+        'HOST': 'lobby.faforever.com',
+        'PORT': 8001,
+        'LOCAL_REPLAY_PORT': 15000,
+        'GAME_PORT_DEFAULT': 6112
+    },
+    'MUMBLE_URL': "mumble://{login}@mumble.faforever.com/Games?version:1.2.0",
+    'FORUMS_URL': "http://forums.faforever.com/forums",
+    'WEBSITE_URL': "http://www.faforever.com",
+    'UNITDB_URL': "http://content.faforever.com/faf/unitsDB/",
+    'WIKI_URL': "http://wiki.faforever.com/mediawiki/index.php/Main_Page",
+    'SUPPORT_URL': "http://forums.faforever.com/forums/viewforum.php?f:3",
+    'TICKET_URL': "http://forums.faforever.com/forums/viewforum.php?f:3",
+    'STEAMLINK_URL': "http://app.faforever.com/faf/steam.php",
+    'PASSWORD_RECOVERY_URL': "http://www.faforever.com/faf/forgotPass.php",
+    'NAME_CHANGE_URL': "http://www.faforever.com/faf/userName.php"
 }
