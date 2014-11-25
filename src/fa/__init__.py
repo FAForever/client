f352aef56ee6ebaaaf66d62338dbdf5e1994e62d
Michael Sondergaard <msond@cs.au.dk>
2014-11-25 22:56:23 +0100

    Add test_play, rename fa.play to fa.run

--- src/fa/__init__.py
+++ src/fa/__init__.py
@@ -31,7 +31,7 @@ DEFAULT_WRITE_GAME_LOG = False
 
 # We only want one instance of Forged Alliance to run, so we use a singleton here (other modules may wish to connect to its signals so it needs persistence)
 from process import instance as instance
-from play import play
+from play import run
 from replay import replay
 
 import check
