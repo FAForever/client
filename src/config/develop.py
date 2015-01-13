6fa789bb55488edf7c67b160ae8cebe8c8132c13
Frozen-byte <Frozen_byte@gmx.de>
2015-01-13 11:42:20 +0100

    removed Redundant code

--- src/config/develop.py
+++ src/config/develop.py
@@ -5,15 +5,7 @@ from os.path import join
 from platform import system
 import logging
 from production import defaults as production_defaults
-
-# These directories are in Appdata (e.g. C:\ProgramData on some Win7 versions)
-if system() != "Windows":
-    #dotFolder for Linux
-    APPDATA_DIR = join(environ['HOME'], ".FAForever")
-elif 'ALLUSERSPROFILE' in environ:
-    APPDATA_DIR = join(environ['ALLUSERSPROFILE'], "FAForever")
-else: 
-    APPDATA_DIR = join(environ['HOME'], "FAForever")
+from production import APPDATA_DIR
 
 defaults = production_defaults.copy()
 
