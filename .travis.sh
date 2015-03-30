c1e6620d9f02231b3ddf776a6adb0d33cb971802
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2015-03-31 01:18:44 +0200

    Fixed wrong order of commands (ldconfig before make install)

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,1 @@
 #!/bin/sh
-
-# Install libgit2 from a bunch of Ubuntu 15.04 binary repos
-mkdir libgit2
-pushd libgit2
-
-wget -q --progress=bar "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-21_0.21.1-1_amd64.deb"
-wget -q --progress=bar "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-dev_0.21.1-1_amd64.deb"
-
-sudo dpkg -i "libgit2-21_0.21.1-1_amd64.deb"
-sudo dpkg -i "libgit2-dev_0.21.1-1_amd64.deb"
-
-popd
