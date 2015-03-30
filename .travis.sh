1d2e6f813b50f5c9e2bd3d24759fee093d065810
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2015-03-31 01:05:09 +0200

    Silencing the output of .travis.sh

--- .travis.sh
+++ .travis.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # Install libgit2 from source and pip-install pygit2
+echo "Installing libgit2"
 wget https://github.com/libgit2/libgit2/archive/v0.22.0.tar.gz
 tar xzf v0.22.0.tar.gz
 cd libgit2-0.22.0/
-cmake .
-make
-sudo make install
-pip install pygit2
+cmake -Wno-dev .
+make --quiet
+sudo make install --quiet
+sudo pip install pygit2
 sudo ldconfig
 python -c 'import pygit2;print("SUCCESS")'
