47d7f2b7c541283059cc0aaf84d2586e982f2be1
Vinzenz Sinapius <Vinzenz.Sinapius@gmail.com>
2015-03-31 00:38:19 +0200

    building libgit2 from source

--- .travis.sh
+++ .travis.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# Install libgit2 from a bunch of Ubuntu 15.04 binary repos
-mkdir libgit2
-pushd libgit2
-
-wget -q --progress=bar "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-21_0.21.1-1_amd64.deb"
-wget -q --progress=bar "https://launchpad.net/ubuntu/+source/libgit2/0.21.1-1/+build/6494190/+files/libgit2-dev_0.21.1-1_amd64.deb"
-
-sudo dpkg -i "libgit2-21_0.21.1-1_amd64.deb"
-sudo dpkg -i "libgit2-dev_0.21.1-1_amd64.deb"
-
-popd
+# Install libgit2 from source and pip-install pygit2
+wget https://github.com/libgit2/libgit2/archive/v0.22.0.tar.gz
+tar xzf v0.22.0.tar.gz
+cd libgit2-0.22.0/
+cmake .
+make
+sudo make install
+pip install pygit2
+sudo ldconfig
+python -c 'import pygit2;print("SUCCESS")'
