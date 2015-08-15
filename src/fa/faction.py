9ec6657ea8e8481fe6ba65d675d910fc0c927dc5
Chris Kitching <chriskitching@linux.com>
2015-08-15 15:24:04 +0100

    A sane enum for representing factions

--- src/fa/faction.py
+++ src/fa/faction.py
@@ -1,22 +1,25 @@
-# -------------------------------------------------------------------------------
-# Copyright (c) 2012 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
+from enum import Enum, unique
 
-UEF = "/uef"
-CYBRAN = "/cybran"
-AEON = "/aeon"
-SERAPHIM = "/seraphim"
+@unique
+class Faction(Enum):
+    """
+    Enum to represent factions. Numbers match up with faction identification ids from the game.
+    """
+    UEF = 1
+    AEON = 2
+    CYBRAN = 3
+    SERAPHIM = 4
+
+    @staticmethod
+    def from_name(name):
+        name = name.lower()
+        if name == "uef":
+            return Faction.UEF
+        elif name == "aeon":
+            return Faction.AEON
+        elif name == "cybran":
+            return Faction.CYBRAN
+        elif name == "seraphim":
+            return Faction.SERAPHIM
+
+        raise ValueError("Invalid faction name provided: %s" % name)
