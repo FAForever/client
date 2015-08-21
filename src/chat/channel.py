5a1d4e5ff987d3aeb0549a29004d0a71fc34ba91
Chris Kitching <chriskitching@linux.com>
2015-08-21 20:46:25 +0100

    Finish stripping copyright messages
    Apparently there's quite a bit of variation in these...

--- src/chat/channel.py
+++ src/chat/channel.py
@@ -1,20 +1,4 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2012 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
+
 from fa.replay import replay
 
 import util
@@ -490,6 +474,3 @@ class Channel(FormClass, BaseClass):
                     self.printMsg(self.lobby.client.login, text, True)
 
         self.chatEdit.clear()
-        
-        
-        
