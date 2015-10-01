6daa26bb2f2741d00d636daae360a30c0fdb511c
Sheeo <sheeo@faforever.com>
2015-10-01 10:57:48 +0200

    Remove player stats dialog
    With it numpy and matplotlib dependencies.

--- src/client/_clientwindow.py
+++ src/client/_clientwindow.py
@@ -34,11 +34,6 @@ import os
 import random
 import notificatation_system as ns
 
-try:
-    from profile import playerstats
-except:
-    pass
-
 FormClass, BaseClass = util.loadUiType("client/client.ui")
 
 
@@ -148,10 +143,6 @@ class ClientWindow(FormClass, BaseClass):
         self.blockSize = 0
 
         self.uniqueId = None
-        try:
-            self.profile = playerstats.Statpage(self)
-        except:
-            pass
 
         self.sendFile = False
         self.progress = QtGui.QProgressDialog()
