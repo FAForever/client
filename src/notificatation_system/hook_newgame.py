0f67d3577c737df751ce8398f03c9b1559c11af9
Sheeo <sheeo@faforever.com>
2015-11-03 17:32:18 +0100

    Fix QSettings usage
    Regression from 9bd15b10475abcb41c8fbf613c9159f0475625f3

--- src/notificatation_system/hook_newgame.py
+++ src/notificatation_system/hook_newgame.py
@@ -32,6 +32,7 @@ class NewGameDialog(FormClass, BaseClass):
         util.settings.beginGroup(self.eventType)
         self.mode = util.settings.value('mode', 'friends')
         util.settings.endGroup()
+        util.settings.endGroup()
 
         self.checkBoxFriends.setCheckState(QtCore.Qt.Checked if self.mode == 'friends' else QtCore.Qt.Unchecked)
         self.parent.mode = self.mode
@@ -40,6 +41,7 @@ class NewGameDialog(FormClass, BaseClass):
         util.settings.beginGroup("notification_system")
         util.settings.beginGroup(self.eventType)
         util.settings.setValue('mode', self.mode)
+        util.settings.endGroup()
         util.settings.endGroup()
         util.settings.sync()
         self.parent.mode = self.mode

FAForever/client

commit 9bd15b10475abcb41c8fbf613c9159f0475625f3

Doing the same thing twice and expecting different results

src/notificatation_system/hook_newgame.py
@@ -32,7 +32,6 @@ class NewGameDialog(FormClass, BaseClass):
         util.settings.beginGroup(self.eventType)
         self.mode = util.settings.value('mode', 'friends')
         util.settings.endGroup()
-        util.settings.endGroup()
 
         self.checkBoxFriends.setCheckState(QtCore.Qt.Checked if self.mode == 'friends' else QtCore.Qt.Unchecked)
         self.parent.mode = self.mode
@@ -41,7 +40,6 @@ class NewGameDialog(FormClass, BaseClass):
         util.settings.beginGroup("notification_system")
         util.settings.beginGroup(self.eventType)
         util.settings.setValue('mode', self.mode)
-        util.settings.endGroup()
         util.settings.endGroup()
         util.settings.sync()
         self.parent.mode = self.mode

src/notificatation_system/hook_useronline.py
@@ -32,7 +32,6 @@ class UserOnlineDialog(FormClass, BaseClass):
         util.settings.beginGroup(self.eventType)
         self.mode = util.settings.value('mode', 'friends')
         util.settings.endGroup()
-        util.settings.endGroup()
 
         if self.mode == 'friends':
             self.radioButtonFriends.setChecked(True)
@@ -44,7 +43,6 @@ class UserOnlineDialog(FormClass, BaseClass):
         util.settings.beginGroup("notification_system")
         util.settings.beginGroup(self.eventType)
         util.settings.setValue('mode', self.mode)
-        util.settings.endGroup()
         util.settings.endGroup()
         util.settings.sync()
         self.parent.mode = self.mode

commit 0f67d3577c737df751ce8398f03c9b1559c11af9

Fix QSettings usage
Regression from 9bd15b10475abcb41c8fbf613c9159f0475625f3

src/notificatation_system/hook_newgame.py
@@ -32,6 +32,7 @@ class NewGameDialog(FormClass, BaseClass):
         util.settings.beginGroup(self.eventType)
         self.mode = util.settings.value('mode', 'friends')
         util.settings.endGroup()
+        util.settings.endGroup()
 
         self.checkBoxFriends.setCheckState(QtCore.Qt.Checked if self.mode == 'friends' else QtCore.Qt.Unchecked)
         self.parent.mode = self.mode
@@ -40,6 +41,7 @@ class NewGameDialog(FormClass, BaseClass):
         util.settings.beginGroup("notification_system")
         util.settings.beginGroup(self.eventType)
         util.settings.setValue('mode', self.mode)
+        util.settings.endGroup()
         util.settings.endGroup()
         util.settings.sync()
         self.parent.mode = self.mode

src/notificatation_system/hook_useronline.py
@@ -32,6 +32,7 @@ class UserOnlineDialog(FormClass, BaseClass):
         util.settings.beginGroup(self.eventType)
         self.mode = util.settings.value('mode', 'friends')
         util.settings.endGroup()
+        util.settings.endGroup()
 
         if self.mode == 'friends':
             self.radioButtonFriends.setChecked(True)
@@ -43,6 +44,7 @@ class UserOnlineDialog(FormClass, BaseClass):
         util.settings.beginGroup("notification_system")
         util.settings.beginGroup(self.eventType)
         util.settings.setValue('mode', self.mode)
+        util.settings.endGroup()
         util.settings.endGroup()
         util.settings.sync()
         self.parent.mode = self.mode