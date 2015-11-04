FAForever/client

commit 5a1d4e5ff987d3aeb0549a29004d0a71fc34ba91

Finish stripping copyright messages
Apparently there's quite a bit of variation in these...

src/fa/play.py
@@ -1,20 +1,3 @@
-# -------------------------------------------------------------------------------
-# Copyright (c) 2012 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
 from fa import mods
 
 from .process import instance

src/chat/_chatwidget.py
@@ -1,21 +1,3 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2012 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
-
 import logging
 logger = logging.getLogger(__name__)
 

src/chat/channel.py
@@ -1,20 +1,4 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2012 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
+
 from fa.replay import replay
 
 import util
@@ -490,6 +474,3 @@ class Channel(FormClass, BaseClass):
                     self.printMsg(self.lobby.client.login, text, True)
 
         self.chatEdit.clear()
-        
-        
-        

src/client/_clientwindow.py
@@ -1,20 +1,3 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2012 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
 from functools import partial
 from client.data.player import Player
 from client.updater import fetchClientUpdate

src/downloadManager/__init__.py
@@ -1,21 +1,3 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2013 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
-
 from PyQt4.QtNetwork import QNetworkAccessManager, QNetworkRequest
 from PyQt4 import QtGui, QtCore
 import urllib2

src/fa/proxies.py
@@ -1,20 +1,4 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2013 Gael Honorez.
-# All rights reserved. This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0
-# which accompanies this distribution, and is available at
-# http://www.gnu.org/licenses/gpl.html
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#-------------------------------------------------------------------------------
+
 
 
 from PyQt4 import QtCore, QtGui, QtNetwork

commit f28ab5ab1399687aaf9b4b53cf08525a3f440273

Yes, disable _defunct_ bug reporter
Regressed in 5a1d4e5ff987d3aeb0549a29004d0a71fc34ba91.

src/fa/play.py
@@ -24,6 +24,9 @@ def build_argument_list(game_info, port, arguments=None):
         arguments.append("/log")
         arguments.append('"' + util.LOG_FILE_GAME + '"')
 
+    # Disable defunct bug reporter
+    arguments.append('/nobugreport')
+
     # live replay
     arguments.append('/savereplay')
     arguments.append('"gpgnet://localhost/' + str(game_info['uid']) + "/" + str(game_info['recorder']) + '.SCFAreplay"')