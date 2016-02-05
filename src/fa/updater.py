5989b728d92651360e4a13998a79a066f525cc9c
Martin Müllenhaupt <muellenhaupt@sikoragmbh.de>
2016-02-05 11:56:22 +0100

    files must be readable on linux

--- src/fa/updater.py
+++ src/fa/updater.py
@@ -396,7 +396,7 @@ class Updater(QtCore.QObject):
                 dst_file = os.path.join(dst_dir, file_)
                 if not os.path.exists(dst_file):
                     shutil.copy(src_file, dst_dir)
-                os.chmod(dst_file, stat.S_IWRITE)   # make all files we were considering writable, because we may need to patch them
+                os.chmod(dst_file, stat.S_IWRITE | stat.S_IREAD)   # make all files we were considering writable, because we may need to patch them
 
     def doUpdate(self):
         """ The core function that does most of the actual update work."""
