3a63cd4c5ce1ec07712612fe639df3bffb0bd02c
Brutus5000 <Brutus5000@gmx.net>
2016-07-18 22:21:23 +0200

    Run appveyor without registry fix (#393)
    * Run appveyor without registry fix
    * Remove registry fix completely
    (build runs without fix, see https://ci.appveyor.com/project/Sheeo/client/build/621)

--- appveyor/install_qt.py
+++ appveyor/install_qt.py
@@ -8,22 +8,6 @@ import os
 import subprocess
 import urllib
 
-
-def fix_registry(python_ver):
-    """Update install path on windows registry so PyQt installation installs at the correct
-    location.
-    python_ver must be "34", "27", etc.
-    """
-    import _winreg as winreg
-    python_dir = r'C:\Python%s' % python_ver
-    print("Fixing registry %s..." % python_ver)
-    assert os.path.isdir(python_dir)
-    registry_key = r'Software\Python\PythonCore\%s.%s' % (python_ver[0], python_ver[1])
-    with winreg.OpenKey(winreg.HKEY_CURRENT_USER,
-                        registry_key, 0,
-                        winreg.KEY_WRITE) as key:
-        winreg.SetValue(key, 'InstallPath', winreg.REG_SZ, python_dir)
-
 base_url = 'http://downloads.sourceforge.net/project/pyqt/'
 downloads = {
     'py34-pyqt5': 'PyQt5/PyQt-5.5/PyQt5-5.5-gpl-Py3.4-Qt5.5.0-x32.exe',
@@ -31,8 +15,6 @@ downloads = {
     'py27-pyqt4': 'PyQt4/PyQt-4.11.4/PyQt4-4.11.4-gpl-Py2.7-Qt4.8.7-x32.exe',
 }
 if 'INSTALL_QT' in os.environ:
-    fix_registry('34')
-    fix_registry('27')
     caption = os.environ['INSTALL_QT']
     url = downloads[caption]
     print("Downloading %s..." % caption)
