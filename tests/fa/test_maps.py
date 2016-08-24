82e3d68580f67e86ab439e15e1d229644ccbfae8
Crotalus <Crotalus@users.noreply.github.com>
2016-08-24 23:14:48 +0200

    Fix broken test with Downloader
    Although the class isn't used anyway...

--- tests/fa/test_maps.py
+++ tests/fa/test_maps.py
@@ -7,5 +7,4 @@ from PyQt4 import QtGui, QtNetwork, QtCore
 TESTMAP_NAME = "faf_test_map"
 
 def test_downloader_has_slot_abort(application):
-    assert callable(maps.Downloader(TESTMAP_NAME, parent=application).abort)
-
+    assert callable(maps.Downloader(TESTMAP_NAME).abort)
