0ce43307959dd93883417b3bec3e451fb62a4ef7
Martin Müllenhaupt <mm+git@netlair.de>
2016-11-10 12:45:59 +0100

    reenable ConnectivityDialog

--- src/client/_clientwindow.py
+++ src/client/_clientwindow.py
@@ -28,7 +28,7 @@ from types import IntType, FloatType, ListType, DictType
 
 from client import ClientState, LOBBY_HOST, \
     LOBBY_PORT, LOCAL_REPLAY_PORT
-
+from connectivity.ConnectivityDialog import ConnectivityDialog
 import logging
 
 logger = logging.getLogger(__name__)
@@ -622,7 +622,10 @@ class ClientWindow(FormClass, BaseClass):
     def closeEvent(self, event):
         logger.info("Close Event for Application Main Window")
         self.saveWindow()
-        self.game_session.close()
+        if getattr(self, "game_session", False):
+            self.game_session.close()
+        if getattr(self, "connectivity_dialog", False):
+            self.connectivity_dialog.close()
 
         if fa.instance.running():
             if QtGui.QMessageBox.question(self, "Are you sure?",
@@ -756,8 +759,8 @@ class ClientWindow(FormClass, BaseClass):
 
     @QtCore.pyqtSlot()
     def connectivityDialog(self):
-        dialog = connectivity.ConnectivityDialog(self.connectivity)
-        dialog.exec_()
+        self.connectivity_dialog = ConnectivityDialog(self.game_session)
+        self.connectivity_dialog.show()
 
     @QtCore.pyqtSlot()
     def linkAbout(self):
--- src/ui/status_logo.py
+++ src/ui/status_logo.py
@@ -42,6 +42,7 @@ class StatusLogo(QLabel):
 
         dc = QAction('Disconnect', None)
         rc = QAction('Reconnect', None)
+        conn = QAction('Connectivity', None)
         about = QAction('About', None)
 
         if self.state != ClientState.DISCONNECTED:
@@ -50,6 +51,7 @@ class StatusLogo(QLabel):
             and self.state != ClientState.RECONNECTING:
             menu.addAction(rc)
 
+        menu.addAction(conn)
         menu.addAction(about)
 
         action = menu.exec_(self.mapToGlobal(event.pos()))
@@ -57,6 +59,8 @@ class StatusLogo(QLabel):
             self.disconnect_requested.emit()
         elif action == rc:
             self.reconnect_requested.emit()
+        elif action == conn:
+            self.connectivity_dialog_requested.emit()
         elif action == about:
             self.about_dialog_requested.emit()
 
