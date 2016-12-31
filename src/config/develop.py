6cf1b0395c817d087fa7e98d6c78d8be1e10f22c
Lukas Erlacher <l.erlacher@googlemail.com>
2016-12-31 19:40:53 +0100

    Global ssl cert verification disable
    Turns off ssl certificate verification globally when in development environment
    to circumvent broken test server config

--- src/config/develop.py
+++ src/config/develop.py
@@ -11,3 +11,8 @@ else:
 
 defaults = production_defaults.copy()
 defaults['host'] = 'test.faforever.com'
+
+# FIXME: Temporary fix for broken https config on test server
+# Turns off certificate verification entirely
+import ssl
+ssl._https_verify_certificates(False)
