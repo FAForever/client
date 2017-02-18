92d09f0c421d742f46301689fcb771cf1bcf0c96
Lukas Erlacher <l.erlacher@googlemail.com>
2017-02-18 11:22:44 +0100

    Hide News Webview until loaded to avoid FOUC
    The "Yanone Kaffeesatz" font used for the news loaded from google fonts
    leads to the news title being invisible until the font is done loading.
    To avoid that, we hide the web view until the first page is done
    loading.

--- src/news/_newswidget.py
+++ src/news/_newswidget.py
@@ -1,6 +1,7 @@
-from PyQt4 import QtCore, QtGui
+from PyQt4 import QtCore, QtGui, QtWebKit
 from PyQt4.QtCore import Qt
 
+import webbrowser
 import util
 import client
 import re
@@ -12,6 +13,30 @@ import base64
 import logging
 
 logger = logging.getLogger(__name__)
+
+class Hider(QtCore.QObject):
+    """
+    Hides a widget by blocking its paint event. This is useful if a
+    widget is in a layout that you do not want to change when the
+    widget is hidden.
+    """
+    def __init__(self, parent=None):
+        super(Hider, self).__init__(parent)
+
+    def eventFilter(self, obj, ev):
+        return ev.type() == QtCore.QEvent.Paint
+
+    def hide(self, widget):
+        widget.installEventFilter(self)
+        widget.update()
+
+    def unhide(self, widget):
+        widget.removeEventFilter(self)
+        widget.update()
+
+    def hideWidget(self, sender):
+        if sender.isWidgetType():
+            self.hide(sender)
 
 FormClass, BaseClass = util.loadUiType("news/news.ui")
 
@@ -68,6 +93,14 @@ class NewsWidget(FormClass, BaseClass):
         self.newsWebView.settings().setUserStyleSheetUrl(QtCore.QUrl(
                 'data:text/css;charset=utf-8;base64,' + base64.b64encode(self.CSS)
             ))
+        # open all links in external browser
+        self.newsWebView.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
+        self.newsWebView.page().linkClicked.connect(self.linkClicked)
+
+        # hide webview until loaded to avoid FOUC
+        self.hider = Hider()
+        self.hider.hide(self.newsWebView)
+        self.newsWebView.loadFinished.connect(self.loadFinished)
 
         self.newsList.setIconSize(QtCore.QSize(0,0))
         self.newsList.setItemDelegate(NewsItemDelegate(self))
@@ -81,3 +114,11 @@ class NewsWidget(FormClass, BaseClass):
             title = current.newsPost['title'],
             content = current.newsPost['body'],
         ))
+
+    def linkClicked(self, url):
+        webbrowser.open(url.toString())
+
+    def loadFinished(self, ok):
+        self.hider.unhide(self.newsWebView)
+        self.newsWebView.loadFinished.disconnect(self.loadFinished)
+
