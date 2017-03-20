2764d38b3d8e65c0f6d4e2c015981a5303fea886
Martin Müllenhaupt <mm+git@netlair.de>
2017-03-20 09:00:34 +0100

    662 make tests run

--- runtests.py
+++ runtests.py
@@ -1,19 +1,7 @@
 #! /usr/bin/env python3
 
-import sip, pytest
-sip.setapi('QString', 2)
-sip.setapi('QVariant', 2)
-sip.setapi('QStringList', 2)
-sip.setapi('QList', 2)
-sip.setapi('QProcess', 2)
-sip.setapi('QDate', 2)
-sip.setapi('QDateTime', 2)
-sip.setapi('QTextStream', 2)
-sip.setapi('QTime', 2)
-sip.setapi('QUrl', 2)
-
+import pytest
 import sys
 
 if __name__ == '__main__':
-    import sys
     sys.exit(pytest.main(sys.argv[1:]))
