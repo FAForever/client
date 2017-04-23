1383dfbeb79a5a5d7b1b1bf3c253365231a31581
Igor Kotrasinski <ikotrasinsk@gmail.com>
2017-04-24 01:17:49 +0200

    Add a game class reflecting game info from server
    The Game class represents an active game on the server. The server can
    send us updated state of a given game, which we will track in a single
    object, broadcasting important events.
    
    Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

---
+++ src/model/game.py
@@ -0,0 +1,169 @@
+from PyQt5.QtCore import QObject, pyqtSignal
+
+from enum import Enum
+from decorators import with_logger
+
+
+class GameState(Enum):
+    OPEN = "open"
+    PLAYING = "playing"
+    CLOSED = "closed"
+
+# This enum has a counterpart on the server
+class GameVisibility(Enum):
+    PUBLIC = "public"
+    FRIENDS = "friends"
+
+# For when we get an inconsistent game update.
+class BadUpdateException(Exception):
+    pass
+
+"""
+Represents a game happening on the server. Updates for the game state are sent
+from the server, identified by game uid. Updates are propagated with signals.
+
+The game with a given uid starts when we receive the first game message and
+ends with some update, or is ended manually. Once the game ends, it can't be
+updated or ended again. Update and game end are propagated with signals.
+
+If an update is malformed / forbidden, an exception is thrown.
+"""
+@with_logger
+class Game(QObject):
+
+    gameUpdated = pyqtSignal()
+    gameClosed = pyqtSignal()
+    newState = pyqtSignal()
+
+    def __init__(self, *args, **kwargs):
+        QObject.__init__(self)
+
+        try:
+            self.uid = (lambda uid, *a, **kw: uid) (*args, **kwargs) # Extract uid from args
+        except TypeError as e:
+            raise BadUpdateException(e.args[0])
+
+        self.state = None
+        self.launched_at = None
+        self.num_players = None
+        self.max_players = None
+        self.title = None
+        self.host = None
+        self.mapname = None
+        self.map_file_path = None
+        self.teams = None
+        self.featured_mod = None
+        self.featured_mod_versions = None
+        self.sim_mods = None
+        self.password_protected = None
+        self.visibility = None
+        self._aborted = False
+
+        self.update(*args, **kwargs)
+
+    def update(self, *args, **kwargs):
+        try:
+            self._update(*args, **kwargs)
+        except TypeError as e:
+            raise BadUpdateException(e.args[0])
+
+    def _update(self,
+        uid,
+        state,
+        launched_at,
+        num_players,
+        max_players,
+        title,
+        host,
+        mapname,
+        map_file_path,
+        teams,
+        featured_mod,
+        featured_mod_versions,
+        sim_mods,
+        password_protected,
+        visibility,
+        command = None,    # Ignored, for convenience since server puts this in the game dict
+        ):
+
+        if self.closed():
+            raise BadUpdateException("Cannot update a closed game {}!".format(self.uid))
+
+        if self.uid != uid:
+            raise BadUpdateException("Trying to update game {} with mismatching uid {}"
+                                     .format(self.uid, uid))
+
+        try:
+            state = GameState(state)
+        except ValueError:
+            raise BadUpdateException("Unknown game state {} for game {}"
+                                     .format(state, self.uid))
+
+        try:
+            visibility = GameVisibility(visibility)
+        except ValueError:
+            raise BadUpdateException("Unknown game {} visibility {}"
+                                     .format(self.uid, visibility))
+
+        if self.launched_at is not None and self.launched_at != launched_at:
+            self._logger.warn("Overwriting launch time for game {}".format(self.uid))
+
+        oldstate = self.state
+        self.launched_at = launched_at
+        self.state = state
+        self.num_players = num_players
+        self.max_players = max_players
+        self.title = title
+        self.host = host
+        self.mapname = mapname
+        self.map_file_path = map_file_path
+
+        # Dict of <teamname> : [list of player names]
+        self.teams = teams
+
+        # Actually a game mode like faf, coop, ladder etc.
+        self.featured_mod = featured_mod
+
+        # Featured mod versions for this game used to update FA before joining
+        # TODO - investigate if this is actually necessary
+        self.featured_mod_versions = featured_mod_versions
+
+        # Dict of mod uid: mod version for each mod used by the game
+        self.sim_mods = sim_mods
+        self.password_protected = password_protected
+        self.visibility = visibility
+
+        self.gameUpdated.emit()
+        if self.state != oldstate:
+            self.newState.emit()
+        if self.closed():
+            self.gameClosed.emit()
+
+    def closed(self):
+        return self.state == GameState.CLOSED or self._aborted
+
+    # Used when the server confuses us whether the game is valid anymore.
+    def abort_game(self):
+        if not self.closed():
+            self._aborted = True
+            self.gameClosed.emit()
+
+    def to_dict(self):
+        return {
+                "uid": self.uid,
+                "state": self.state,
+                "launched_at": self.launched_at,
+                "num_players": self.num_players,
+                "max_players": self.max_players,
+                "title": self.title,
+                "host": self.host,
+                "mapname": self.mapname,
+                "map_file_path": self.map_file_path,
+                "teams": self.teams,
+                "featured_mod": self.featured_mod,
+                "featured_mod_versions": self.featured_mod_versions,
+                "sim_mods": self.sim_mods,
+                "password_protected": self.password_protected,
+                "visibility": self.visibility,
+                "command": "game_info" # For compatibility
+            }
---
+++ tests/unit_tests/model/test_game.py
@@ -0,0 +1,148 @@
+import pytest
+import copy
+
+from model import game
+
+DEFAULT_DICT = {
+    "uid":  1,
+    "state": game.GameState.OPEN,
+    "launched_at": 10000,
+    "num_players": 3,
+    "max_players": 8,
+    "title": "Sentons sucks",
+    "host":  "IllIIIlIlIIIlI",
+    "mapname": "Sentons Ultimate 6v6",
+    "map_file_path": "xrca_co_000001.scfamap",
+    "teams": {
+        1: ["IllIIIlIlIIIlI", "TableNoob"],
+        2: ["Kraut"]
+        },
+    "featured_mod": "faf",
+    "featured_mod_versions": {},
+    "sim_mods": {},
+    "password_protected": False,
+    "visibility": game.GameVisibility.PUBLIC,
+}
+
+def test_simple_correct_init():
+    g = game.Game(**DEFAULT_DICT)
+
+def test_uid_required_for_init():
+    data = copy.deepcopy(DEFAULT_DICT)
+    del data["uid"]
+    with pytest.raises(game.BadUpdateException):
+        g = game.Game(**data)
+
+def test_wrong_uid_for_update():
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    data["uid"] = 2
+    with pytest.raises(game.BadUpdateException):
+        g.update(**data)
+
+def general_invalid_dict(dict_):
+    with pytest.raises(game.BadUpdateException):
+        g = game.Game(**dict_)
+    g = game.Game(**DEFAULT_DICT)
+    with pytest.raises(game.BadUpdateException):
+        g.update(**dict_)
+
+def test_invalid_gamestate():
+    data = copy.deepcopy(DEFAULT_DICT)
+    data["state"] = "AAAAA"
+    general_invalid_dict(data)
+
+def test_invalid_visibility():
+    data = copy.deepcopy(DEFAULT_DICT)
+    i = 0
+    while i in game.GameVisibility:
+        i += 1
+    data["visibility"] = i
+    general_invalid_dict(data)
+
+def test_update_signal(mocker):
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    updated = mocker.Mock()
+    g.gameUpdated.connect(updated)
+    data["host"] = "OtherName"
+    g.update(**data)
+    assert updated.called
+
+def test_close_signals(mocker):
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    updated = mocker.Mock()
+    g.gameUpdated.connect(updated)
+    closed = mocker.Mock()
+    g.gameClosed.connect(closed)
+    data["state"] = game.GameState.CLOSED
+    g.update(**data)
+    assert updated.called
+    assert closed.called
+
+def test_closed_determined_by_gamestate():
+    data = copy.deepcopy(DEFAULT_DICT)
+
+    g = game.Game(**data)
+    assert not g.closed()
+    g.update(**data)
+    assert not g.closed()
+    data["state"] = game.GameState.PLAYING
+    g.update(**data)
+    assert not g.closed()
+    data["state"] = game.GameState.CLOSED
+    g.update(**data)
+    assert g.closed()
+
+def test_abort_closes(mocker):
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    closed = mocker.Mock()
+    g.gameClosed.connect(closed)
+    assert not g.closed()
+
+    g.abort_game()
+    assert g.closed()
+    assert closed.called
+
+def test_cannot_update_closed():
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    g.abort_game()
+    with pytest.raises(game.BadUpdateException):
+        g.update(data)
+
+def test_state_change_signals(mocker):
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    change = mocker.Mock()
+    g.newState.connect(change)
+
+    data["state"] = game.GameState.PLAYING
+    g.update(**data)
+    change.assert_called_with(g)
+    change.reset_mock()
+
+    data["state"] = game.GameState.CLOSED
+    g.update(**data)
+    change.assert_called_with(g)
+
+def test_abort_does_not_change_state(mocker):
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    change = mocker.Mock()
+    g.newState.connect(change)
+
+    g.abort_game()
+    assert not change.called
+
+def test_no_state_changes(mocker):
+    data = copy.deepcopy(DEFAULT_DICT)
+    g = game.Game(**data)
+    change = mocker.Mock()
+    g.newState.connect(change)
+    data['title'] = "New"
+    g.update(**data)
+
+    assert not change.called
