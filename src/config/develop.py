8e54cc3a0f13ed5993e9aa8c6214afa80ad80511
Igor Kotrasinski <ikotrasinsk@gmail.com>
2017-04-23 14:32:02 +0200

    Remove the SSL disable workaround
    Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

--- src/config/develop.py
+++ src/config/develop.py
@@ -1,5 +1,4 @@
 import os
-import logging
 
 from .production import defaults as production_defaults
 
@@ -12,8 +11,3 @@ else:
 defaults = production_defaults.copy()
 defaults['host'] = 'test.faforever.com'
 defaults['client/logs/console'] = True
-
-# FIXME: Temporary fix for broken https config on test server
-# Turns off certificate verification entirely
-import ssl
-ssl._https_verify_certificates(False)
