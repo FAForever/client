4c2dcc246f34b50eb2c33d1c2e36fd286ba5f0d2
Igor Kotrasinski <ikotrasinsk@gmail.com>
2017-04-29 18:39:14 +0200

    Add a simple file downloader class
    Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

--- src/downloadManager/__init__.py
+++ src/downloadManager/__init__.py
@@ -9,6 +9,106 @@ from config import Settings
 
 logger= logging.getLogger(__name__)
 
+
+class FileDownload(object):
+    """
+    A simple async one-shot file downloader.
+    """
+    def __init__(self, nam, addr, dest, start=lambda _: None, progress=lambda _: None, finished=lambda _: None):
+        self._nam = nam
+        self.addr = addr
+        self.dest = dest
+
+        self.canceled = False
+        self.error = False
+
+        self.blocksize = 8192
+        self.bytes_total = 0
+        self.bytes_progress = 0
+
+        self._dfile = None
+
+        self.cb_start = start
+        self.cb_progress = progress
+        self.cb_finished = finished
+
+        self._reading = False
+        self._running = False
+        self._sock_finished = False
+
+    def _stop(self):
+        ran = self._running
+        self._running = False
+        if ran:
+            self._finish()
+
+    def _error(self):
+        self.error = True
+        self._stop()
+
+    def cancel(self):
+        self.canceled = True
+        self._stop()
+
+    def _finish(self):
+        if self._dfile is not None:
+            self._dfile.close()
+            self._dfile = None
+        self.cb_finished(self)
+
+    def run(self):
+        self._running = True
+        req = QNetworkRequest(QtCore.QUrl(self.addr))
+        req.setRawHeader(b'User-Agent', b"FAF Client")
+        req.setAttribute(QNetworkRequest.FollowRedirectsAttribute, True)
+        req.setMaximumRedirectsAllowed(3)
+
+        self.cb_start(self)
+
+        self._dfile = self._nam.get(req)
+        self._dfile.error.connect(self._error)
+        self._dfile.finished.connect(self._atFinished)
+        self._dfile.downloadProgress.connect(self._atProgress)
+        self._dfile.readyRead.connect(self._kick_read)
+        self._kick_read()
+
+    def _atFinished(self):
+        self._sock_finished = True
+        self._kick_read()
+
+    def _atProgress(self, recv, total):
+        self.bytes_progress = recv
+        self.bytes_total = total
+
+    def _kick_read(self):    # Don't run the read loop more than once at a time
+        if self._reading:
+            return
+        self._reading = True
+        self._read()
+        self._reading = False
+
+    def _read(self):
+        while self._dfile.bytesAvailable() > 0 and self._running:
+            self._readloop()
+        if self._sock_finished:
+            # Sock can be marked as finished either before read or inside readloop
+            # Either way we've read everything after it was marked
+            self._stop()
+
+    def _readloop(self):
+            bs = self.blocksize if self.blocksize is not None else self._dfile.bytesAvailable() 
+            self.dest.write(self._dfile.read(bs))
+            self.cb_progress(self)
+
+    def succeeded(self):
+        return not self.error and not self.canceled
+
+    def waitForCompletion(self):
+        waitFlag = QtCore.QEventLoop.WaitForMoreEvents
+        while self._running:
+            QtWidgets.QApplication.processEvents(waitFlag)
+
+
 VAULT_PREVIEW_ROOT = "{}/faf/vault/map_previews/small/".format(Settings.get('content/host'))
 
 class downloadManager(QtCore.QObject):
