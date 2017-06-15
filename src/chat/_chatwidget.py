FAForever/client

commit 5a8ac3c4a1ec43c87dadfd66c8c7907101e66c26

Cleanup resize behaviour for client / channel
I think there was once an optimization attempt, with resize signals
from the client. It definitely don't work since the client eats a lot
of CPU while resizing anyway, signals are only used in one place and
cause some timers to potentially get called a hundred times a second,
Let's clean this up and worry about resize performance some other day.

Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

src/chat/_chatwidget.py
@@ -128,6 +128,11 @@ class ChatWidget(FormClass, BaseClass, SimpleIRCClient):
                     self.channels[channel].chatters[player].avatarItem.setIcon(QtGui.QIcon(util.respix(reply.url().toString())))
                     self.channels[channel].chatters[player].avatarItem.setToolTip(self.channels[channel].chatters[player].avatarTip)
 
+    def addChannel(self, name, channel):
+        self.channels[name] = channel
+        self.addTab(self.channels[name], name)
+        channel.setTextWidth()
+
     def closeChannel(self, index):
         """
         Closes a channel tab.
@@ -184,8 +189,7 @@ class ChatWidget(FormClass, BaseClass, SimpleIRCClient):
             return False
 
         if name not in self.channels:
-            self.channels[name] = Channel(self, name, True)
-            self.addTab(self.channels[name], name)
+            self.addChannel(name, Channel(self, name, True))
 
             # Add participants to private channel
             self.channels[name].addChatter(name, id)
@@ -194,7 +198,6 @@ class ChatWidget(FormClass, BaseClass, SimpleIRCClient):
         if activate:
             self.setCurrentWidget(self.channels[name])
 
-        self.channels[name].resizing()
         return True
 
     @QtCore.pyqtSlot(list)
@@ -272,17 +275,16 @@ class ChatWidget(FormClass, BaseClass, SimpleIRCClient):
 
         # If we're joining, we need to open the channel for us first.
         if channel not in self.channels:
-            self.channels[channel] = Channel(self, channel)
+            newch = Channel(self, channel)
             if channel.lower() in self.crucialChannels:
-                self.insertTab(1, self.channels[channel], channel)  # CAVEAT: This is assumes a server tab exists.
-                self.client.localBroadcast.connect(self.channels[channel].printRaw)
-                self.channels[channel].printAnnouncement("Welcome to Forged Alliance Forever!", "red", "+3")
-                self.channels[channel].printAnnouncement("Check out the wiki: http://wiki.faforever.com for help with common issues.", "white", "+1")
-                self.channels[channel].printAnnouncement("", "black", "+1")
-                self.channels[channel].printAnnouncement("", "black", "+1")
+                self.insertTab(1, newch, channel)  # CAVEAT: This is assumes a server tab exists.
+                self.client.localBroadcast.connect(newch.printRaw)
+                newch.printAnnouncement("Welcome to Forged Alliance Forever!", "red", "+3")
+                newch.printAnnouncement("Check out the wiki: http://wiki.faforever.com for help with common issues.", "white", "+1")
+                newch.printAnnouncement("", "black", "+1")
+                newch.printAnnouncement("", "black", "+1")
 
-            else:
-                self.addTab(self.channels[channel], channel)
+            self.addChannel(channel, newch)
 
             if channel.lower() in self.crucialChannels:  # Make the crucial channels not closeable, and make the last one the active one
                 self.setCurrentWidget(self.channels[channel])
@@ -295,7 +297,6 @@ class ChatWidget(FormClass, BaseClass, SimpleIRCClient):
             # TODO: search better solution, that html in nick & channel no rendered
             self.client.notificationSystem.on_event(ns.Notifications.USER_ONLINE,
                                                     {'user': id, 'channel': channel})
-        self.channels[channel].resizing()
 
     def on_part(self, c, e):
         channel = e.target()

src/chat/channel.py
@@ -100,11 +100,6 @@ class Channel(FormClass, BaseClass):
         self.chatEdit.returnPressed.connect(self.sendLine)
         self.chatEdit.setChatters(self.chatters)
 
-        self.lobby.client.doneresize.connect(self.resizing)
-
-        self.resizeTimer = QtCore.QTimer(self)
-        self.resizeTimer.timeout.connect(self.canresize)
-
     def joinChannel(self, index):
         """ join another channel """
         channel = self.channelsComboBox.itemText(index)
@@ -118,13 +113,12 @@ class Channel(FormClass, BaseClass):
         if keyevent.key() == 67:
             self.chatArea.copy()
 
-    def canresize(self):
-        if self.isVisible():
-            self.chatArea.setLineWrapColumnOrWidth(self.chatArea.size().width() - 20)  # Hardcoded, but seems to be enough (tabstop was a bit large)
-            self.resizeTimer.stop()
+    def resizeEvent(self, size):
+        BaseClass.resizeEvent(self, size)
+        self.setTextWidth()
 
-    def resizing(self):
-        self.resizeTimer.start(10)
+    def setTextWidth(self):
+        self.chatArea.setLineWrapColumnOrWidth(self.chatArea.size().width() - 20)  # Hardcoded, but seems to be enough (tabstop was a bit large)
 
     def showEvent(self, event):
         self.stopBlink()

src/client/_clientwindow.py
@@ -101,9 +101,6 @@ class ClientWindow(FormClass, BaseClass):
     state_changed = QtCore.pyqtSignal(object)
     authorized = QtCore.pyqtSignal(object)
 
-    # This signal is emitted when the client is done rezising
-    doneresize = QtCore.pyqtSignal()
-
     # These signals notify connected modules of game state changes (i.e. reasons why FA is launched)
     viewingReplay = QtCore.pyqtSignal(QtCore.QUrl)
 
@@ -187,11 +184,6 @@ class ClientWindow(FormClass, BaseClass):
         self.lobby_dispatch["update"] = self.handle_update
         self.lobby_dispatch["welcome"] = self.handle_welcome
         self.lobby_dispatch["authentication_failed"] = self.handle_authentication_failed
-
-        # Timer for resize events
-        self.resizeTimer = QtCore.QTimer(self)
-        self.resizeTimer.timeout.connect(self.resized)
-        self.preferedSize = 0
 
         # Process used to run Forged Alliance (managed in module fa)
         fa.instance.started.connect(self.startedFA)
@@ -708,13 +700,6 @@ class ClientWindow(FormClass, BaseClass):
                 return
 
         return QtGui.QMainWindow.closeEvent(self, event)
-
-    def resizeEvent(self, size):
-        self.resizeTimer.start(400)
-
-    def resized(self):
-        self.resizeTimer.stop()
-        self.doneresize.emit()
 
     def initMenus(self):
         self.actionLink_account_to_Steam.triggered.connect(partial(self.open_url, Settings.get("STEAMLINK_URL")))

commit 18fd1bc2a831b12b506d3724f4a6edd2f294452c

Fix wrong channel order caused by 5a8ac3c4a1ec
Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

src/chat/_chatwidget.py
@@ -128,10 +128,12 @@ class ChatWidget(FormClass, BaseClass, SimpleIRCClient):
                     self.channels[channel].chatters[player].avatarItem.setIcon(QtGui.QIcon(util.respix(reply.url().toString())))
                     self.channels[channel].chatters[player].avatarItem.setToolTip(self.channels[channel].chatters[player].avatarTip)
 
-    def addChannel(self, name, channel):
+    def addChannel(self, name, channel, index = None):
         self.channels[name] = channel
-        self.addTab(self.channels[name], name)
-        channel.setTextWidth()
+        if index is None:
+            self.addTab(self.channels[name], name)
+        else:
+            self.insertTab(index, self.channels[name], name)
 
     def closeChannel(self, index):
         """
@@ -277,14 +279,14 @@ class ChatWidget(FormClass, BaseClass, SimpleIRCClient):
         if channel not in self.channels:
             newch = Channel(self, channel)
             if channel.lower() in self.crucialChannels:
-                self.insertTab(1, newch, channel)  # CAVEAT: This is assumes a server tab exists.
+                self.addChannel(channel, newch, 1)  # CAVEAT: This is assumes a server tab exists.
                 self.client.localBroadcast.connect(newch.printRaw)
                 newch.printAnnouncement("Welcome to Forged Alliance Forever!", "red", "+3")
                 newch.printAnnouncement("Check out the wiki: http://wiki.faforever.com for help with common issues.", "white", "+1")
                 newch.printAnnouncement("", "black", "+1")
                 newch.printAnnouncement("", "black", "+1")
-
-            self.addChannel(channel, newch)
+            else:
+                self.addChannel(channel, newch)
 
             if channel.lower() in self.crucialChannels:  # Make the crucial channels not closeable, and make the last one the active one
                 self.setCurrentWidget(self.channels[channel])

src/chat/channel.py
@@ -122,6 +122,7 @@ class Channel(FormClass, BaseClass):
 
     def showEvent(self, event):
         self.stopBlink()
+        self.setTextWidth()
         return BaseClass.showEvent(self, event)
 
     @QtCore.pyqtSlot()