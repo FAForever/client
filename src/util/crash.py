f0f5b076a09a711ae5e02c549aba36b86682cc95
Igor Kotrasinski <ikotrasinsk@gmail.com>
2017-06-17 20:01:03 +0200

    Simplify crash dialog
    Current crash dialog has auto reporting capability - the problem is,
    nobody uses it, it uses old Python api with synchronous requests and
    adds an unwieldy dependency on faftools which are no longer really
    shared code. Simplify the dialog and give it a .ui layout.
    
    Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

--- src/util/crash.py
+++ src/util/crash.py
@@ -4,120 +4,44 @@ import traceback
 import util
 from config import Settings
 
-from . import APPDATA_DIR, PERSONAL_DIR, VERSION_STRING, LOG_FILE_FAF, \
-    readlines
+from . import APPDATA_DIR, PERSONAL_DIR, VERSION_STRING
 
-from PyQt4 import QtGui, QtCore
-from faf.api import BugReportSchema
-from faf.domain.bugs import BugReport, BugReportTarget
+from PyQt4.QtGui import QDesktopServices
+from PyQt4.QtCore import QUrl
 
 CRASH_REPORT_USER = "pre-login"
 
+FormClass, BaseClass = util.loadUiType("client/crash.ui")
 
-class CrashDialog(QtGui.QDialog):
+
+class CrashDialog(FormClass, BaseClass):
     def __init__(self, exc_info, *args, **kwargs):
-        QtGui.QDialog.__init__(self, *args, **kwargs)
+        BaseClass.__init__(self, *args, **kwargs)
+        self.setupUi(self)
 
-        exc_type, exc_value, traceback_object = exc_info
+        trace = "".join(traceback.format_exception(*exc_info, limit=10))
 
-        dialog = self
-        if kwargs.get('automatic'):
-            automatic = True
-        else:
-            automatic = Settings.get('client/auto_bugreport', type=bool, default=True)
-
-        self.trace = "".join(traceback.format_exception(exc_type, exc_value, traceback_object, 10))
-
-        if automatic:
-            self.title = "Bug report"
-        else:
-            self.title = "Report from " + CRASH_REPORT_USER + u": " + str(exc_value)
-
-        self.bugreport_target = BugReportTarget('client',
-                                                'https://github.com/FAForever/client',
-                                                config.VERSION)
-        self.bugreport = BugReport(self.title,
-                                   target=self.bugreport_target,
-                                   traceback=self.trace,
-                                   automatic=automatic)
-
-        dialog.setWindowTitle(self.title)
-
-        description = u""
         try:
-            description += (u"\n**FAF Username:** " + CRASH_REPORT_USER)
-            description += (u"\n**FAF Version:** " + VERSION_STRING)
-            description += (u"\n**FAF Environment:** " + config.environment)
-            description += (u"\n**FAF Directory:** " + APPDATA_DIR)
-            description += (u"\n**FA Path:** " + str(util.settings.value("ForgedAlliance/app/path", None, type=str)))
-            description += (u"\n**Home Directory:** " + PERSONAL_DIR)
-        except StandardError:
-            description += (u"\n**(Exception raised while writing debug vars)**")
+            desc = []
+            desc.append(("FAF Username", CRASH_REPORT_USER))
+            desc.append(("FAF Version", VERSION_STRING))
+            desc.append(("FAF Environment", config.environment))
+            desc.append(("FAF Directory", APPDATA_DIR))
+            fa_path = util.settings.value("ForgedAlliance/app/path",
+                                          "Unknown",
+                                          type=str)
+            desc.append(("FA Path: ", fa_path))
+            desc.append(("Home Directory", PERSONAL_DIR))
 
-        log = u""
-        try:
-            log += (u"\n".join(readlines(LOG_FILE_FAF, False)[-128:]))
-        except StandardError:
-            log += (unicode(LOG_FILE_FAF))
-            log += (u"empty or not readable")
+            desc = "".join(["{}: {}\n".format(n, d) for n, d in desc])
+        except Exception:
+            desc = "(Exception raised while writing runtime info)\n"
 
-        self.bugreport.description = description
-        self.bugreport.log = log
+        self.logField.setText("{}\nRuntime info:\n\n{}".format(trace, desc))
 
-        if not automatic:
-            self.box = QtGui.QTextEdit()
-            self.box.setFont(QtGui.QFont("Lucida Console", 8))
-            self.box.append(description)
-            dialog.layout().addWidget(self.box)
-            dialog.setLayout(QtGui.QVBoxLayout())
-            label = QtGui.QLabel()
-            label.setText(
-                    "An error has occurred in the FAF client. <br><br>You can report it by clicking the ticket button.")
-            label.setWordWrap(True)
-            dialog.layout().addWidget(label)
+        self.helpButton.clicked.connect(self.tech_support)
+        self.continueButton.clicked.connect(self.accept)
+        self.quitButton.clicked.connect(self.reject)
 
-            label = QtGui.QLabel()
-            label.setText("<b>This is what happened. If you have more to add please write in the field below.</b>")
-            label.setWordWrap(False)
-            dialog.layout().addWidget(label)
-            self.sendButton = QtGui.QPushButton("\nReport error\n")
-            self.sendButton.pressed.connect(self.post_report)
-
-            dialog.layout().addWidget(self.sendButton)
-
-            label = QtGui.QLabel()
-            label.setText("<b></b><br/><i>(please note that the error may be fatal, proceed at your own risk)</i>")
-            label.setWordWrap(False)
-            dialog.layout().addWidget(label)
-
-            self.buttons = QtGui.QDialogButtonBox()
-            buttons = self.buttons
-            buttons.addButton("Continue", QtGui.QDialogButtonBox.AcceptRole)
-            buttons.addButton("Close FAF", QtGui.QDialogButtonBox.RejectRole)
-            buttons.addButton("Help", QtGui.QDialogButtonBox.HelpRole)
-            buttons.accepted.connect(dialog.accept)
-            buttons.rejected.connect(dialog.reject)
-            buttons.helpRequested.connect(self.tech_support)
-            dialog.layout().addWidget(buttons)
-        else:
-            dialog.setLayout(QtGui.QVBoxLayout())
-            label = QtGui.QLabel()
-            label.setText("An error has occurred in the FAF client. It has been automatically reported.")
-            label.setWordWrap(True)
-            dialog.layout().addWidget(label)
-            self.post_report()
-            self.sendButton = QtGui.QPushButton("\nOK\n")
-            self.sendButton.pressed.connect(dialog.accept)
-            dialog.layout().addWidget(self.sendButton)
-
-    @QtCore.pyqtSlot()
     def tech_support(self):
-        QtGui.QDesktopServices().openUrl(QtCore.QUrl(Settings.get("HELP_URL")))
-
-    @QtCore.pyqtSlot()
-    def post_report(self):
-        # Be pessimistic here and import a new api client
-        import faf.api.client
-        c = faf.api.client.ApiClient()
-        data, errors = BugReportSchema().dump(self.bugreport)
-        c.post('/bugs', json=data)
+        QDesktopServices().openUrl(QUrl(Settings.get("SUPPORT_URL")))
