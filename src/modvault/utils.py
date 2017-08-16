FAForever/client

commit c71b7092d5daaa46ca75f07a0554b617be9741fc

Make selected Mods independent of joined games
Introduces extra setting in `FA Lobby.ini` to save mods activated by
user.
Also reduces some logspam that annoyed me while debugging.

Fixes #161

src/modvault/utils.py
@@ -23,6 +23,8 @@ MODVAULT_DOWNLOAD_ROOT = "{}/faf/vault/".format(Settings.get('content/host'))
 
 installedMods = [] # This is a global list that should be kept intact. So it should be cleared using installedMods[:] = []
 
+selectedMods = Settings.get('play/mods', default=[]) # mods selected by user, are not overwritten by temporary mods selected when joining game
+
 class ModInfo(object):
     def __init__(self, **kwargs):
         self.name = "Not filled in"
@@ -104,7 +106,7 @@ def fullPathToIcon(path):
 def getIcon(name):
     img = os.path.join(util.CACHE_DIR, name)
     if os.path.isfile(img):
-        logger.debug("Using cached preview image for: " + name)
+        logger.log(5, "Using cached preview image for: " + name)
         return img
     return None
 
@@ -185,27 +187,31 @@ def getModInfoFromFolder(modfolder): # modfolder must be local to MODFOLDER
     modCache[modfolder] = m
     return m
 
-def getActiveMods(uimods=None): # returns a list of ModInfo's containing information of the mods
+def getActiveMods(uimods=None, temporary=True): # returns a list of ModInfo's containing information of the mods
     """uimods:
         None - return all active mods
         True - only return active UI Mods
         False - only return active non-UI Mods
+       temporary:
+        read from game.prefs and not from settings
     """
     active_mods = []
     try:
         if not os.path.exists(PREFSFILENAME):
             logger.info("No game.prefs file found")
             return []
-        
-        l = luaparser.luaParser(PREFSFILENAME)
-        l.loweringKeys = False
-        modlist = l.parse({"active_mods":"active_mods"},{"active_mods":{}})["active_mods"]
-        if l.error:
-            logger.info("Error in reading the game.prefs file")
-            return []
-        uids = [uid for uid,b in modlist.items() if b == 'true']
-        #logger.debug("Active mods detected: %s" % str(uids))
-        
+        if temporary:
+            l = luaparser.luaParser(PREFSFILENAME)
+            l.loweringKeys = False
+            modlist = l.parse({"active_mods":"active_mods"},{"active_mods":{}})["active_mods"]
+            if l.error:
+                logger.info("Error in reading the game.prefs file")
+                return []
+            uids = [uid for uid,b in modlist.items() if b == 'true']
+            #logger.debug("Active mods detected: %s" % str(uids))
+        else:
+            uids = selectedMods[:]
+
         allmods = []
         for m in installedMods:
             if ((uimods == True and m.ui_only) or (uimods == False and not m.ui_only) or uimods == None):
@@ -217,23 +223,33 @@ def getActiveMods(uimods=None): # returns a list of ModInfo's containing informa
         return []
     
 
-def setActiveMods(mods, keepuimods=True): #uimods works the same as in getActiveMods
+def setActiveMods(mods, keepuimods=True, temporary=True): #uimods works the same as in getActiveMods
     """
     keepuimods:
         None: Replace all active mods with 'mods'
         True: Keep the UI mods already activated activated
         False: Keep only the non-UI mods that were activated activated
         So set it True if you want to set gameplay mods, and False if you want to set UI mods.
+    temporary:
+        Set this when mods are activated due to joining a game.
     """
     if keepuimods != None:
         keepTheseMods = getActiveMods(keepuimods) # returns the active UI mods if True, the active non-ui mods if False
     else:
         keepTheseMods = []
     allmods = keepTheseMods + mods
+    logger.debug('setting active Mods: {}'.format([mod.uid for mod in allmods]))
     s = "active_mods = {\n"
     for mod in allmods:
         s += "['%s'] = true,\n" % str(mod.uid)
     s += "}"
+
+    if not temporary:
+        logger.debug('selectedMods was: {}'.format(Settings.get('play/mods')))
+        selectedMods = list([str(mod.uid) for mods in allmods])
+        logger.debug('Writing selectedMods: {}'.format(selectedMods))
+        Settings.set('play/mods', selectedMods)
+        logger.debug('selectedMods written: {}'.format(Settings.get('play/mods')))
 
     try:
         f = open(PREFSFILENAME, 'r')

src/fa/maps.py
@@ -436,7 +436,7 @@ def preview(mapname, pixmap=False):
         for extension in iconExtensions:
             img = os.path.join(util.CACHE_DIR, mapname + "." + extension)
             if os.path.isfile(img):
-                logger.debug("Using cached preview image for: " + mapname)
+                logger.log(5,"Using cached preview image for: " + mapname)
                 return util.icon(img, False, pixmap)
 
         # Try to find in local map folder

src/games/hostgamewidget.py
@@ -77,7 +77,7 @@ class HostgameWidget(FormClass, BaseClass):
             self.mods[mod.totalname] = mod
             self.modList.addItem(mod.totalname)
 
-        names = [mod.totalname for mod in modvault.getActiveMods(uimods=False)]
+        names = [mod.totalname for mod in modvault.getActiveMods(uimods=False, temporary=False)]
         logger.debug("Active Mods detected: %s" % str(names))
         for name in names:
             l = self.modList.findItems(name, QtCore.Qt.MatchExactly)
@@ -122,7 +122,7 @@ class HostgameWidget(FormClass, BaseClass):
 
         modnames = [str(moditem.text()) for moditem in self.modList.selectedItems()]
         mods = [self.mods[modstr] for modstr in modnames]
-        modvault.setActiveMods(mods, True)
+        modvault.setActiveMods(mods, True, False)
 
         self.parent.client.host_game(title=self.title,
                                  mod=self.featured_mod,

src/util/__init__.py
@@ -427,7 +427,7 @@ def iconUnit(unitname):
 
     img = os.path.join(CACHE_DIR, unitname)
     if os.path.isfile(img):
-        logger.debug("Using cached preview image for: " + unitname)
+        logger.log(5, "Using cached preview image for: " + unitname)
         return icon(img, False)
     # Try to download from web
     img = __downloadPreviewFromWeb(unitname)

commit 9c922957a223f59a12d5d2c3f05917e8e9babc8a

Fix not saving mod choices for hosted game properly
A typo and a missing global declaration in c71b7092d caused only one mod
to be saved, and only after closing and reopening the client. Easy fix.

Fixes #840.

Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

src/modvault/utils.py
@@ -262,8 +262,9 @@ def setActiveMods(mods, keepuimods=True, temporary=True):  # uimods works the sa
     s += "}"
 
     if not temporary:
+        global selectedMods
         logger.debug('selectedMods was: {}'.format(Settings.get('play/mods')))
-        selectedMods = list([str(mod.uid) for mods in allmods])
+        selectedMods = [str(mod.uid) for mod in allmods]
         logger.debug('Writing selectedMods: {}'.format(selectedMods))
         Settings.set('play/mods', selectedMods)
         logger.debug('selectedMods written: {}'.format(Settings.get('play/mods')))