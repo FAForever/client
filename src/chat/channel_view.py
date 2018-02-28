01a2dee16632bc174b03c50acaa1bb2a067cd0a8
Igor Kotrasinski <ikotrasinsk@gmail.com>
2018-02-28 21:34:27 +0100

    Add dedicated class for handling chatter list
    Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

--- src/chat/channel_view.py
+++ src/chat/channel_view.py
@@ -7,20 +7,12 @@ class ChannelView:
     def __init__(self, channel, controller, map_preview_dler, avatar_dler):
         self._channel = channel
         self._controller = controller
-        self._map_preview_dler = map_preview_dler
-        self._avatar_dler = avatar_dler
-
         self.widget = ChannelWidget(channel.id_key)
-        self._delegate = build_delegate(QSize(150, 30), self.widget)
-        self.widget.set_chatter_delegate(self._delegate)
-        self.widget.set_chatter_tooltips(self._delegate.tooltip)
-
-        model = ChatterModel(channel, map_preview_dler, avatar_dler)
-        self.widget.set_chatter_model(model)
+        self._chatter_list = ChatterList(self.widget, map_preview_dler,
+                                         avatar_dler)
 
         self.widget.line_typed.connect(self._at_line_typed)
         self.widget.set_autocompletion_source(self._channel)
-        self.widget.chatter_list_resized.connect(self._update_chatter_width)
         channel.lines.added.connect(self._add_line)
 
     def _add_line(self, number):
@@ -29,6 +21,21 @@ class ChannelView:
 
     def _at_line_typed(self, line):
         self._controller.send_message(self._channel.id_key, line)
+
+
+class ChatterList:
+    def __init__(self, widget, map_previev_dler, avatar_dler):
+        self._widget = widget
+        self._map_preview_dler = map_preview_dler
+        self._avatar_dler = avatar_dler
+
+        self._delegate = build_delegate(QSize(150, 30), self._widget)
+        self._widget.set_chatter_delegate(self._delegate)
+        self._widget.set_chatter_tooltips(self._delegate.tooltip)
+
+        model = ChatterModel(channel, map_preview_dler, avatar_dler)
+        self.widget.set_chatter_model(model)
+        self.widget.chatter_list_resized.connect(self._update_chatter_width)
 
     def _update_chatter_width(self, size):
         self._delegate.update_width(size)
