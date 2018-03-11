0e651f2a02a60382bd719ce323b6efd1b22579b2
Igor Kotrasinski <ikotrasinsk@gmail.com>
2018-03-11 18:11:55 +0100

    Add time to chat lines
    Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

--- src/model/chat/chatline.py
+++ src/model/chat/chatline.py
@@ -1,4 +1,10 @@
+import time
+
+
 class ChatLine:
-    def __init__(self, sender, text):
+    def __init__(self, sender, text, timestamp=None):
         self.sender = sender
         self.text = text
+        if timestamp is None:
+            timestamp = time.time()
+        self.time = timestamp
