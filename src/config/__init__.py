f9ceacd210ac8dc33624c6f77332f0699b410123
Igor Kotrasinski <ikotrasinsk@gmail.com>
2018-05-10 21:23:04 +0200

    Reformat config __init__.py
    Signed-off-by: Igor Kotrasinski <ikotrasinsk@gmail.com>

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -15,7 +15,9 @@ if sys.platform == 'win32':
     import ctypes
     from . import admin
 
-_settings = QtCore.QSettings(QtCore.QSettings.IniFormat, QtCore.QSettings.UserScope, "ForgedAllianceForever", "FA Lobby")
+_settings = QtCore.QSettings(QtCore.QSettings.IniFormat,
+                             QtCore.QSettings.UserScope,
+                             "ForgedAllianceForever", "FA Lobby")
 _unpersisted_settings = {}
 
 CONFIG_PATH = os.path.dirname(_settings.fileName())
@@ -57,7 +59,8 @@ class Settings:
             _settings.remove(key)
 
     @staticmethod
-    def persisted_property(key, default_value=None, persist_if=lambda self: True, type=str):
+    def persisted_property(key, default_value=None,
+                           persist_if=lambda self: True, type=str):
         """
         Create a magically persisted property
 
@@ -68,9 +71,10 @@ class Settings:
         :param type: Type of values for persisting
         :return: a property suitable for a class
         """
-        return property(lambda s: Settings.get(key, default=default_value, type=type),
-                        lambda s, v: Settings.set(key, v, persist=persist_if(s)),
-                        doc='Persisted property: {}. Default: '.format(key, default_value))
+        return property(
+            lambda s: Settings.get(key, default=default_value, type=type),
+            lambda s, v: Settings.set(key, v, persist=persist_if(s)),
+            doc='Persisted property: {}. Default: '.format(key, default_value))
 
     @staticmethod
     def sync():
@@ -90,8 +94,12 @@ def set_data_path_permissions():
     Set the owner of C:\ProgramData\FAForever recursively to the current user
     """
     if not admin.isUserAdmin():
-        win32api.MessageBox(0, "FA Forever needs to fix folder permissions due to user change. Please confirm the following two admin prompts.", "User changed")
-    if sys.platform == 'win32' and (not 'CI' in os.environ):
+        win32api.MessageBox(
+            0,
+            ("FA Forever needs to fix folder permissions due to user change. "
+             "Please confirm the following two admin prompts."),
+            "User changed")
+    if sys.platform == 'win32' and ('CI' not in os.environ):
         data_path = Settings.get('client/data_path')
         if os.path.exists(data_path):
             my_user = win32api.GetUserNameEx(win32con.NameSamCompatible)
@@ -104,28 +112,37 @@ def check_data_path_permissions():
     Checks if the current user is owner of C:\ProgramData\FAForever
     Fixes the permissions in case that FAF was run as different user before
     """
-    if sys.platform == 'win32' and (not 'CI' in os.environ):
+    if sys.platform == 'win32' and ('CI' not in os.environ):
         data_path = Settings.get('client/data_path')
         if os.path.exists(data_path):
             try:
                 my_user = win32api.GetUserNameEx(win32con.NameSamCompatible)
-                sd = win32security.GetFileSecurity(data_path, win32security.OWNER_SECURITY_INFORMATION)
+                sd = win32security.GetFileSecurity(
+                    data_path, win32security.OWNER_SECURITY_INFORMATION)
                 owner_sid = sd.GetSecurityDescriptorOwner()
-                name, domain, type = win32security.LookupAccountSid(None, owner_sid)
+                name, domain, type = win32security.LookupAccountSid(
+                    None, owner_sid)
                 data_path_owner = "%s\\%s" % (domain, name)
 
                 if my_user != data_path_owner:
                     set_data_path_permissions()
             except Exception as e:
-                # we encountered error 1332 in win32security.LookupAccountSid here: http://forums.faforever.com/viewtopic.php?f=3&t=13728
-                # https://msdn.microsoft.com/en-us/library/windows/desktop/aa379166(v=vs.85).aspx states:
-                # "It also occurs for SIDs that have no corresponding account name, such as a logon SID that identifies a logon session."
-                # so let's just fix permissions on every exception for now and wait for someone stuck in a permission-loop
-                win32api.MessageBox(0,
-                                    "FA Forever ran into an exception checking the data folder permissions: '{}'\n"
-                                    "If you get this popup more than one time, please report a screenshot of this popup to tech support forum. "
-                                    "Full stacktrace:\n{}".format(e, traceback.format_exc()),
-                                    "Permission check exception")
+                # we encountered error 1332 in win32security.LookupAccountSid
+                # here: http://forums.faforever.com/viewtopic.php?f=3&t=13728
+                # msdn.microsoft.com/en-us/library/windows/desktop/aa379166.aspx
+                # states:
+                # "It also occurs for SIDs that have no corresponding account
+                # name, such as a logon SID that identifies a logon session."
+                # so let's just fix permissions on every exception for now and
+                # wait for someone stuck in a permission-loop
+                win32api.MessageBox(
+                    0,
+                    "FA Forever ran into an exception "
+                    "checking the data folder permissions: '{}'\n"
+                    "If you get this popup more than one time, please report "
+                    "a screenshot of this popup to tech support forum. "
+                    "Full stacktrace:\n{}".format(e, traceback.format_exc()),
+                    "Permission check exception")
                 set_data_path_permissions()
 
 
@@ -149,7 +166,9 @@ def make_dirs():
                 set_data_path_permissions()
                 os.makedirs(path)
 
-VERSION = version.get_release_version(dir=fafpath.get_resdir(), git_dir=fafpath.get_srcdir())
+
+VERSION = version.get_release_version(dir=fafpath.get_resdir(),
+                                      git_dir=fafpath.get_srcdir())
 
 
 def is_development_version():
@@ -167,6 +186,7 @@ def is_beta():
 
 # TODO: move stuff below to Settings __init__ once we make it an actual object
 
+
 if _settings.contains('client/force_environment'):
     environment = _settings.value('client/force_environment', 'development')
 
@@ -177,7 +197,7 @@ elif environment == 'development':
 
 for k, v in defaults.items():
     if isinstance(v, str):
-        defaults[k] = v.format(host = Settings.get('host'))
+        defaults[k] = v.format(host=Settings.get('host'))
 
 
 def os_language():
@@ -207,19 +227,25 @@ if not Settings.contains('client/language'):
 # Setup normal rotating log handler
 make_dirs()
 
+
 def setup_file_handler(filename):
-    # check permissions of writing the log file first (which fails when changing users)
+    # check permissions of writing the log file first
+    # (which fails when changing users)
     log_file = os.path.join(Settings.get('client/logs/path'), filename)
     try:
-        with open(log_file, "a") as f:
+        with open(log_file, "a"):
             pass
     except IOError as e:
         set_data_path_permissions()
-    rotate = RotatingFileHandler(os.path.join(Settings.get('client/logs/path'), filename),
-                                 maxBytes=int(Settings.get('client/logs/max_size')),
-                                 backupCount=1)
-    rotate.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(name)-30s %(message)s'))
-    return MemoryHandler(int(Settings.get('client/logs/buffer_size')), target=rotate)
+    rotate = RotatingFileHandler(
+        os.path.join(Settings.get('client/logs/path'), filename),
+        maxBytes=int(Settings.get('client/logs/max_size')),
+        backupCount=1)
+    rotate.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)-8s %(name)-30s %(message)s'))
+    return MemoryHandler(int(Settings.get('client/logs/buffer_size')),
+                         target=rotate)
+
 
 client_handler = setup_file_handler('forever.log')
 
@@ -229,8 +255,10 @@ logging.getLogger().setLevel(Settings.get('client/logs/level', type=int))
 if Settings.get('client/logs/console', False, type=bool):
     # Setup logging output to console
     devh = logging.StreamHandler()
-    devh.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(name)-30s %(message)s'))
+    devh.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)-8s %(name)-30s %(message)s'))
     logging.getLogger().addHandler(devh)
     logging.getLogger().setLevel(Settings.get('client/logs/level', type=int))
 
-logging.getLogger().info("FAF version: {} Environment: {}".format(VERSION, environment))
+logging.getLogger().info("FAF version: {} Environment: {}".format(
+    VERSION, environment))
