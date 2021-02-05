6cad3119d4579d7d427f345dff36e615b2eed052
gatsik <74517072+Gatsik@users.noreply.github.com>
2021-02-05 17:05:13 +0200

    Use mean rating instead of estimate for map pools fix tmm url ensure that pool is int
    Signed-off-by: gatsik <74517072+Gatsik@users.noreply.github.com>

--- src/games/_gameswidget.py
+++ src/games/_gameswidget.py
@@ -253,7 +253,7 @@ class GamesWidget(FormClass, BaseClass):
         if self._me.id is None:
             QDesktopServices.openUrl(QUrl(Settings.get("MAPPOOL_URL")))
         else:
-            pool = (self.client.players[self._me.id].ladder_estimate() + 700) // 500
+            pool = int((self.client.players[self._me.id].ladder_rating_mean + 700) // 500)
             if pool < 1:
                 pool = 1
             elif pool > 5:
@@ -264,9 +264,9 @@ class GamesWidget(FormClass, BaseClass):
     
     def startViewTmmMapsPool(self):
         if self._me.id is None:
-            QDesktopServices.openUrl(QUrl("https://forum.faforever.com/topic/854/team-matchmaker-beta-release-now-available")) #still has no proper link on forum
+            QDesktopServices.openUrl(QUrl(Settings.get("MAPPOOL_URL")))
         else:
-            pool = (self.client.players[self._me.id].tmm_estimate + 700) // 500
+            pool = int((self.client.players[self._me.id].tmm_rating_mean + 700) // 500)
             if pool < 1:
                 pool = 1
             elif pool > 5:
