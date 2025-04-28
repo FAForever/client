44b906f138d237e9424d78cce2b893e4811a69f7
Gatsik <74517072+Gatsik@users.noreply.github.com>
2025-04-28 20:41:21 +0300

    Drop fractional part of floats in replay duration

--- src/replays/replaydetails/helpers.py
+++ src/replays/replaydetails/helpers.py
@@ -2,5 +2,8 @@ def seconds_to_human(seconds: int, *, sep: str = ":", full: bool = True) -> str:
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
     if full:
-        return f"{h}{sep}{m:02}{sep}{s:02}"
-    return f"{h}h{sep}{m:02}m{sep}{s:02}s" if h else f"{m}m{sep}{s:02}s" if m else f"{s}s"
+        return f"{h}{sep}{m:02.0f}{sep}{s:02.0f}"
+    h_str = f"{h}h{sep}" if h else ""
+    m_str = f"{m:02.0f}m{sep}" if m else ""
+    s_str = f"{s:02.0f}s"
+    return f"{h_str}{m_str}{s_str}"
